d404e72f7327fa137a3ffa7f704b6f7475159274
Bridget Almas <balmas@users.noreply.github.com>
2018-01-04 20:26:21 -0500

    Merge pull request #12 from alpheios-project/featuresort
    add lemmatext property to definition

--- packages/data-models/dist/alpheios-data-models.js
+++ packages/data-models/dist/alpheios-data-models.js
@@ -405,14 +405,15 @@ var constants = Object.freeze({
 });
 
 class Definition {
-  constructor (text, language, format) {
+  constructor (text, language, format, lemmaText) {
     this.text = text;
     this.language = language;
     this.format = format;
+    this.lemmaText = lemmaText;
   }
 
   static readObject (jsonObject) {
-    return new Definition(jsonObject.text, jsonObject.language, jsonObject.format)
+    return new Definition(jsonObject.text, jsonObject.language, jsonObject.format, jsonObject.lemmaText)
   }
 }
 
--- packages/data-models/src/definition.js
+++ packages/data-models/src/definition.js
@@ -1,12 +1,13 @@
 class Definition {
-  constructor (text, language, format) {
+  constructor (text, language, format, lemmaText) {
     this.text = text
     this.language = language
     this.format = format
+    this.lemmaText = lemmaText
   }
 
   static readObject (jsonObject) {
-    return new Definition(jsonObject.text, jsonObject.language, jsonObject.format)
+    return new Definition(jsonObject.text, jsonObject.language, jsonObject.format, jsonObject.lemmaText)
   }
 }
 export default Definition
