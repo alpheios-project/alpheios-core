a8ebe8274ae196a74e59fb091c194fd766892c0e
Kirlat <kirlat@yula.media>
2018-01-30 21:27:27 +0400

    Updated ViewSet to return part of speech dependent view list.

--- packages/inflection-tables/views/lib/view-set.js
+++ packages/inflection-tables/views/lib/view-set.js
@@ -3,19 +3,36 @@ import GreekViews from '../lang/greek.js'
 import * as Models from 'alpheios-data-models'
 
 export default class ViewSet {
-  constructor () {
+  constructor (inflectionData = undefined) {
     this.views = new Map()
     this.views.set(Models.Constants.LANG_LATIN, LatinViews)
     this.views.set(Models.Constants.LANG_GREEK, GreekViews)
-  }
+    this.inflectionData = inflectionData
+    this.matchingViews = []
 
-  getViews (inflectionData) {
     if (this.views.has(inflectionData.languageID)) {
-      return this.views.get(inflectionData.languageID)
+      this.matchingViews = this.views.get(inflectionData.languageID)
         .filter(view =>
           inflectionData[Models.Feature.types.part].includes(view.partOfSpeech) &&
           view.enabledForLexemes(inflectionData.homonym.lexemes))
     }
-    return []
+
+    this.partsOfSpeech = []
+    this.partOfSpeechViews = {}
+    for (const view of this.matchingViews) {
+      if (!this.partsOfSpeech.includes(view.partOfSpeech)) {
+        this.partsOfSpeech.push(view.partOfSpeech)
+        this.partOfSpeechViews[view.partOfSpeech] = []
+      }
+      this.partOfSpeechViews[view.partOfSpeech].push(view)
+    }
+  }
+
+  getViews (partOfSpeech = undefined) {
+    if (this.partsOfSpeech.includes(partOfSpeech)) {
+      return this.partOfSpeechViews[partOfSpeech]
+    } else {
+      return []
+    }
   }
 }
