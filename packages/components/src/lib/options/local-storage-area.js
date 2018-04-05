31f6fe9168d113e7bff7faf26b159ce55e93052a
Kirlat <kirlat@yula.media>
2018-04-05 11:42:08 +0400

    LocalStorageArea class got minor updates

--- packages/components/src/lib/options/local-storage-area.js
+++ packages/components/src/lib/options/local-storage-area.js
@@ -1,24 +1,32 @@
 /**
- * A set of wrapper functions with same signatures as the ones from `browser.storage`.
- * Useful in situations when local storage functionality required to be a drop-in replacement of `browser.storage`.
+ * A set of wrapper functions with signatures similar to the ones in `browser.storage`.
+ * Useful in situations when local storage functionality is used
+ * as a drop-in replacement for `browser.storage` functions.
+ *
+ * Local storage can contain multiple key-value pairs. get() with an empty parameter
+ * should return all keys related to the particular object (e.g. set of options). A set of those keys
+ * is defined by the `storage domain` concept. A key named `storage-domain-name-key` is saved to the
+ * local storage along with its key-value pairs. This special key
+ * will contain an array of keys that belong to the storage domain. Those key values
+ * are used then to retrieve all relevant keys from the storage.
  */
 export default class LocalStorageArea {
-  static get storageDomain () {
-    return 'alpheios-storage-domain'
-  }
-
   /**
    * A wrapper around a local storage `setItem()` function.
-   * It allows to store a several objects to local storage at once.
-   * A wrapper signature approximates `browser.storage.set()` behavior, but may not match it exactly.
-   * @param {string} storageDomain
+   * It allows to store one or several key-value pairs to local storage.
+   * A wrapper signature approximates `browser.storage.set()` behavior, except for the first `storageDomain`
+   * parameter. It's better to be set with `bind()`.
+   * @param {string} storageDomain - A name of a storage domain where key-value pairs should be stored.
    * @param {object} keysObject - An object containing one or more key/value pairs to be stored in storage.
-   * If an item already exists, its value will be updated.
+   * If a particular item already exists, its value will be updated.
    * @return {Promise} - A promise that is resolved with with a void value if all key/value pairs are stored
    * successfully. If at least on save operation fails, returns a rejected promise with an error information.
    */
-  static set (storageDomain = LocalStorageArea.storageDomain, keysObject) {
+  static set (storageDomain, keysObject) {
     return new Promise((resolve, reject) => {
+      if (!storageDomain) {
+        reject(new Error(`Storage domain is not provided`))
+      }
       try {
         let keys = window.localStorage.getItem(`${storageDomain}-keys`)
         if (keys) {
@@ -26,10 +34,10 @@ export default class LocalStorageArea {
         } else {
           keys = [] // No keys in storage yet
         }
-        for (const [key, value] in Object.entries(keysObject)) {
+        for (const [key, value] of Object.entries(keysObject)) {
           window.localStorage.setItem(key, value)
           if (!keys.includes(key)) {
-            keys.push(keys)
+            keys.push(key)
           }
         }
         // Save a list of keys to the local storage
@@ -44,17 +52,22 @@ export default class LocalStorageArea {
   /**
    * A wrapper around a local storage `getItem()` function. It retrieves one or several values from
    * local storage.
-   * A wrapper signature approximates `browser.storage.get()` behavior, but may not match it exactly.
-   * @param {string} storageDomain
+   * A wrapper signature approximates `browser.storage.get()` behavior, except for the first `storageDomain`
+   * parameter. It's better to be set with `bind()`.
+   * @param {string} storageDomain - A name of a storage domain from where the key-value pairs
+   * should be retrieved (required only if keys are not provided).
    * @param {string | Array | object | null | undefined } keys - A key (string)
    * or keys (an array of strings or an object) to identify the item(s) to be retrieved from storage.
    * If you pass an empty string, object or array here, an empty object will be retrieved. If you pass null,
    * or an undefined value, the entire storage contents will be retrieved.
-   * @return {Promise} A Promise that will be fulfilled with a results object containing every object in keys
-   * that was found in the storage area. If the operation failed, the promise will be rejected with an error message.
+   * @return {Promise} A Promise that will be fulfilled with a results object containing key-value pairs
+   * found in the storage area. If this operation failed, the promise will be rejected with an error message.
    */
-  static get (storageDomain = LocalStorageArea.storageDomain, keys = undefined) {
+  static get (storageDomain, keys = undefined) {
     return new Promise((resolve, reject) => {
+      if (!keys && !storageDomain) {
+        reject(new Error(`Storage domain is not provided`))
+      }
       try {
         if (!keys) {
           keys = []
@@ -68,6 +81,7 @@ export default class LocalStorageArea {
           keys = []
         }
 
+        let result = {}
         if (keys.length === 0) {
           // If no keys specified, will retrieve all values
           keys = window.localStorage.getItem(`${storageDomain}-keys`)
@@ -75,11 +89,12 @@ export default class LocalStorageArea {
             keys = JSON.parse(keys)
           } else {
             // Nothing to retrieve
-            reject(new Error('Unable to get data because no keys provided or no keys listed in local storage'))
+            console.log(`Unable to retrieve data for "${storageDomain}" storage domain because no keys provided or no keys listed in local storage`)
+            console.log(`This might be normal for devices where no data is saved to the local storage yet`)
+            resolve(result)
           }
         }
 
-        let result = {}
         for (const key of keys) {
           result[key] = window.localStorage.getItem(key)
         }
