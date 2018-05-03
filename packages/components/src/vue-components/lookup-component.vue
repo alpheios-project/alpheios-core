879e476b09b1c7e2285ce52ee693816ada3ea633
irina060981 <irina060981@yandex.ru>
2018-05-03 14:26:47 +1000

    styles fixes

--- packages/components/src/vue-components/lookup-component.vue
+++ packages/components/src/vue-components/lookup-component.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="alpheios-lookup_form">
 		<input class="uk-input lookup_input" type="text" placeholder="Type text">
-		<button class="uk-button uk-button-primary uk-button-small alpheios-popup__more-btn" type="button" tabindex="-1">Lookup</button>
+		<button class="uk-button uk-button-primary uk-button-small" type="button" tabindex="-1">Lookup</button>
 	</div>
 </template>
 <script>
@@ -15,12 +15,15 @@
     @import "../styles/alpheios";
 
     .alpheios-lookup_form {
-    	margin: 15px 10px;
+    	margin: 15px 10px 5px;
+    	text-align: center;
 
     	.uk-input {
 	    	width: 80%;
 		    line-height: 28px;
 		    height: 30px;
+		    font-size: 14px;
+		    margin-bottom: 10px;
 		}
     }
 </style>
