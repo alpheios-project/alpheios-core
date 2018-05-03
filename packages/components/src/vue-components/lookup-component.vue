66c9cc443bcc5120b815158b5deb55ecdaa789f9
irina060981 <irina060981@yandex.ru>
2018-05-03 14:21:49 +1000

    adding a component for lookup

--- packages/components/src/vue-components/lookup-component.vue
+++ packages/components/src/vue-components/lookup-component.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="alpheios-lookup_form">
-		<input class="uk-input" type="text" placeholder="Type text">
-		<button class="uk-button uk-button-default" type="button" tabindex="-1">Lookup</button>
+		<input class="uk-input lookup_input" type="text" placeholder="Type text">
+		<button class="uk-button uk-button-primary uk-button-small alpheios-popup__more-btn" type="button" tabindex="-1">Lookup</button>
 	</div>
 </template>
 <script>
@@ -13,5 +13,15 @@
 </script>
 <style lang="scss">
     @import "../styles/alpheios";
+
+    .alpheios-lookup_form {
+    	margin: 15px 10px;
+
+    	.uk-input {
+	    	width: 80%;
+		    line-height: 28px;
+		    height: 30px;
+		}
+    }
 </style>
 
