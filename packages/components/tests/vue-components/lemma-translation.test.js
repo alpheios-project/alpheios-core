aa4cd462e2d4113c0bdaf77b6bdb86372de9934b
irina060981 <irina060981@yandex.ru>
2018-05-03 19:07:05 +1000

    fixing pull request conflict

--- packages/components/tests/vue-components/lemma-translation.test.js
+++ packages/components/tests/vue-components/lemma-translation.test.js
@@ -6,6 +6,7 @@ describe('lemma-translation.test.js', () => {
   let lemmaKey = 'capio-lat-verb-3rd-very frequent-Ox.Lat.Dict.-transitive'
   let translations = {}
   translations[lemmaKey] = {glosses: ['taking/seizing; [usus ~ => getting ownership by continued possession];']}
+  let translationsNoValue = {}
 
   it('show hasValue for lemmakey with translations', () => {
     let cmpWithValue = mount(LemmaTranslation, {
@@ -27,5 +28,16 @@ describe('lemma-translation.test.js', () => {
     })
 
     expect(cmpWithValue.find('.alpheios-lemma__translations .hasValue').text()).toEqual('taking/seizing; [usus ~ => getting ownership by continued possession];')
+  })
+
+  it('do not show for lemmakey with empty translations', () => {
+    let cmpNoValue = mount(LemmaTranslation, {
+      propsData: {
+        lemmakey: lemmaKey,
+        translations: translationsNoValue
+      }
+    })
+
+    expect(cmpNoValue.find('.alpheios-lemma__translations .hasValue').exists()).toBeFalsy()
   })
 }) // Create a copy of the original component with full values
