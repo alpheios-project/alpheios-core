af34ff71bcc41369d087b5641473bb429a6cc11a
Bridget Almas <balmas@users.noreply.github.com>
2018-05-04 12:28:22 -0400

    Merge pull request #39 from alpheios-project/node-build
    Node build

---
+++ packages/inflection-tables/build/config.mjs
@@ -0,0 +1,42 @@
+import path from 'path'
+const projectRoot = process.cwd()
+const webpack = {
+  common: {
+    context: projectRoot,
+    entry: './index.js',
+    externals: {
+      'intl-messageformat': 'intl-messageformat',
+      'alpheios-data-models': 'alpheios-data-models',
+      'uuid': 'uuid'
+    },
+    resolve:{
+      alias: {
+        'alpheios-data-models': path.join(projectRoot, 'node_modules/alpheios-data-models/dist/alpheios-data-models.js')
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.csv$/,
+          use: ['raw-loader'],
+          enforce: 'pre'
+        },
+        {
+          test: /\.json$/,
+          use: ['raw-loader'],
+          enforce: 'pre',
+          type: 'javascript/auto' // To prevent running Webpack's default JSON parser on the output of raw-loader
+        }
+      ]
+    }
+  },
+
+  production: {
+    output: {filename: 'alpheios-inflection-tables.min.js'}
+  },
+  development: {
+    output: {filename: 'alpheios-inflection-tables.js'}
+  }
+}
+
+export { webpack }
