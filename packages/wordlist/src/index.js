4b807c895711e47949f950189ab324e252218fc9
Sklyarova Irina <irina060981@users.noreply.github.com>
2019-02-12 10:12:07 +1000

    Merge pull request #8 from alpheios-project/fix-upload-messages-l10n
    Export locales messages

--- packages/wordlist/src/index.js
+++ packages/wordlist/src/index.js
@@ -4,4 +4,6 @@ import WordlistController from '@/controllers/wordlist-controller.js'
 import WordListPanel from '@/vue-components/word-list-panel.vue'
 import UserDataManager from '@/controllers/user-data-manager.js'
 
-export { Style, WordlistController, WordListPanel, UserDataManager }
+import EnUs from '@/locales/en-us/messages.json'
+
+export { Style, WordlistController, WordListPanel, UserDataManager, EnUs }
