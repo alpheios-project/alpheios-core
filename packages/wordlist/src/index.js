fd6aeae6131d4be85b50b548d00e4f1e5440cacd
Sklyarova Irina <irina060981@users.noreply.github.com>
2019-04-04 17:32:37 +1000

    Merge pull request #16 from alpheios-project/fix-wordlist-issues
    Remove vue-components, styles and icons

--- packages/wordlist/src/index.js
+++ packages/wordlist/src/index.js
@@ -1,9 +1,4 @@
-import Style from './styles/style.scss'
-
 import WordlistController from '@/controllers/wordlist-controller.js'
-import WordListPanel from '@/vue-components/word-list-panel.vue'
 import UserDataManager from '@/controllers/user-data-manager.js'
 
-import EnUs from '@/locales/en-us/messages.json'
-
-export { Style, WordlistController, WordListPanel, UserDataManager, EnUs }
+export { WordlistController, UserDataManager }
