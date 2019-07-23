cbf5dac7e47225a4c95fd30eb52598f923d0e9a7
Bridget Almas <balmas@users.noreply.github.com>
2019-07-23 15:00:27 -0400

    Merge pull request #692 from alpheios-project/issue-679
    fix #679

--- packages/components/src/vue/components/user-auth.vue
+++ packages/components/src/vue/components/user-auth.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <login/>
-    <div class="alpheios-user-auth__user-info-box" v-show="this.$store.state.auth.isAuthenticated">
-      <div class="alpheios-user-auth__user-info-item-box">
-        <div class="alpheios-user-auth__user-info-item-name">
-          {{ l10n.getMsg(`AUTH_PROFILE_NICKNAME_LABEL`) }}:
-        </div>
-        <div class="alpheios-user-auth__user-info-item-value">
-          {{ this.$store.state.auth.userNickName ? this.$store.state.auth.userNickName: `&mdash;` }}
+  <div class="alpheios-user-auth">
+    <div class="alpheios-user-auth__user-container">
+      <login/>
+      <div class="alpheios-user-auth__user-info-box" v-show="this.$store.state.auth.isAuthenticated">
+        <div class="alpheios-user-auth__user-info-item-box">
+          <div class="alpheios-user-auth__user-info-item-name">
+            {{ l10n.getMsg(`AUTH_PROFILE_NICKNAME_LABEL`) }}:
+          </div>
+          <div class="alpheios-user-auth__user-info-item-value">
+            {{ this.$store.state.auth.userNickName ? this.$store.state.auth.userNickName: `&mdash;` }}
+          </div>
         </div>
       </div>
+    </div>
+    <div class="alpheios-user-auth__credits">
+      <a width="150" height="50" href="https://auth0.com/?utm_source=oss&utm_medium=gp&utm_campaign=oss"
+        target="_blank" alt="Single Sign On & Token Based Authentication - Auth0">
+        <img width="150" height="50" alt="JWT Auth for open source projects" src="//cdn.auth0.com/oss/badges/a0-badge-dark.png"/></a>
     </div>
   </div>
 </template>
@@ -31,6 +38,13 @@ export default {
 </script>
 <style lang="scss">
   @import "../../styles/variables";
+
+   .alpheios-user-auth {
+     display: flex;
+     flex-direction: column;
+     height: 100%;
+     justify-content: space-between;
+   }
 
   .alpheios-user-auth__user-info-box {
     margin-top: uisize(20px);
