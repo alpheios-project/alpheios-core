51360da5f1202d5289ff4f4004b89bca481401da
Bridget Almas <balmas@users.noreply.github.com>
2019-11-06 14:44:58 -0500

    Merge pull request #854 from alpheios-project/issue-848-fix
    another fix for #848

--- packages/components/src/vue/components/infl-attribute.vue
+++ packages/components/src/vue/components/infl-attribute.vue
@@ -90,7 +90,7 @@ export default {
 </script>
 <style lang="scss">
   @import "../../styles/variables";
-  .alpheios-morph__attr {
+  span.alpheios-morph__attr {
     font-weight: normal;
     padding-right: .25em;
   }
