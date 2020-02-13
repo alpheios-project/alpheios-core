9097a9e706b662e824db5106a4a2a5bb110ef6a7
Kirlat <kirlat@yula.media>
2020-02-13 14:43:13 +0400

    Eliminated unneeded dependencies

--- packages/data-models/build/config-node.mjs
+++ packages/data-models/build/config-node.mjs
@@ -2,17 +2,16 @@ const webpack = {
   common: {
     entry: './driver.js',
     externals: {
-      'intl-messageformat': 'intl-messageformat',
       'uuid/v4': 'uuid/v4'
     },
-    target: "node"
+    target: 'node'
   },
 
   production: {
-    output: {filename: 'alpheios-data-models.node.min.js'}
+    output: { filename: 'alpheios-data-models.node.min.js' }
   },
   development: {
-    output: {filename: 'alpheios-data-models.node.js'}
+    output: { filename: 'alpheios-data-models.node.js' }
   }
 }
 
