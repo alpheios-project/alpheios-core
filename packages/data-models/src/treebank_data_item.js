2bf6ab8d191b584d8f077486084a5c5116e7b3cc
Bridget Almas <balmsa@gmail.com>
2020-02-21 14:42:06 -0500

    add a treebank data item to data models

---
+++ packages/data-models/src/treebank_data_item.js
@@ -0,0 +1,25 @@
+export default class TreebankDataItem  {
+
+  constructor(elem) {
+    const tbSrcElem = elem.ownerDocument.querySelector('[data-alpheios_tb_src]')
+    if (! tbSrcElem) {
+      throw new Error(`No treebank source url defined in the document`)
+    }
+    this.version = tbSrcElem.dataset.alpheios_tb_version ? parseInt(tbSrcElem.dataset.alpheios_tb_version) : null
+    if (! elem.dataset.alpheios_tb_ref) {
+      throw new Error(`Missing treebank reference ${elem.dataset.alpheios_tb_ref}`)
+    }
+    const [doc, ref] = elem.dataset.alpheios_tb_ref.split(/#/)
+    if (!doc || !ref) {
+      throw new Error(`Invalid treebank reference ${elem.dataset.alpheios_tb_ref}`)
+    }
+    const [s, w] = ref.split(/-/)
+    this.sentenceId = s
+    this.wordId = w
+    this.fullUrl = tbSrcElem.dataset.alpheios_tb_src.replace('DOC', doc).replace('SENTENCE', s).replace('WORD', w)
+  }
+
+  get provider() {
+    return new URL(this.fullUrl).origin
+  }
+}
---
+++ packages/data-models/tests/treebank_data_item.test.js
@@ -0,0 +1,72 @@
+/* eslint-env jest */
+/* eslint-disable no-unused-vars */
+import TreebankDataItem from '../src/treebank_data_item'
+
+describe('treebank_data_item.test.js', () => {
+  console.error = function () {}
+  console.log = function () {}
+  console.warn = function () {}
+
+  beforeAll(async () => {
+  })
+
+  beforeEach(() => {
+    jest.spyOn(console, 'error')
+    jest.spyOn(console, 'log')
+    jest.spyOn(console, 'warn')
+  })
+
+  afterEach(() => {
+    jest.resetModules()
+  })
+
+  afterAll(() => {
+    jest.clearAllMocks()
+  })
+
+  it('1 TreebankDataItem - constructor throws error on missing source Element', () => {
+    expect(function () {
+      let wdElem = document.createElement("span")
+      wdElem.setAttribute("data-alpheios_tb_ref","treebank#1-1")
+      document.body.appendChild(wdElem)
+      let item = new TreebankDataItem(wdElem)
+    }).toThrowError(/No treebank source url/)
+  })
+
+  it('2 TreebankDataItem - constructor throws error on missing reference', () => {
+    expect(function () {
+      let srcElem = document.createElement("div")
+      srcElem.setAttribute("data-alpheios_tb_src","http://example.org/DOC/SENTENCE/WORD")
+      document.body.appendChild(srcElem)
+      let wdElem = document.createElement("span")
+      document.body.appendChild(wdElem)
+      let item = new TreebankDataItem(wdElem)
+    }).toThrowError(/Missing treebank reference/)
+  })
+
+  it('3 TreebankDataItem - constructor throws error on invalid reference', () => {
+    expect(function () {
+      let srcElem = document.createElement("div")
+      srcElem.setAttribute("data-alpheios_tb_src","http://example.org/DOC/SENTENCE/WORD")
+      document.body.appendChild(srcElem)
+      let wdElem = document.createElement("span")
+      wdElem.setAttribute("data-alpheios_tb_ref","treebank")
+      document.body.appendChild(wdElem)
+      let item = new TreebankDataItem(wdElem)
+    }).toThrowError(/Invalid treebank reference/)
+  })
+
+  it('4 TreebankDataItem - constructs full item', () => {
+    let srcElem = document.createElement("div")
+    srcElem.setAttribute("data-alpheios_tb_src","http://example.org/DOC/SENTENCE/WORD")
+    document.body.appendChild(srcElem)
+    let wdElem = document.createElement("span")
+    wdElem.setAttribute("data-alpheios_tb_ref","treebank#1-1")
+    document.body.appendChild(wdElem)
+    let item = new TreebankDataItem(wdElem)
+    expect(item.provider).toEqual("http://example.org")
+    expect(item.fullUrl).toEqual("http://example.org/treebank/1/1")
+    expect(item.sentenceId).toEqual("1")
+    expect(item.wordId).toEqual("1")
+  })
+})
