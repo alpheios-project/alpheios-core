7181014d544dd0cada44eee27212703b9bd02e6e
Bridget Almas <balmsa@gmail.com>
2020-03-30 17:01:07 -0400

    use new treebank meta attributes so that pages can support this release of Alpheios as well as the prior

--- packages/data-models/src/treebank_data_item.js
+++ packages/data-models/src/treebank_data_item.js
@@ -1,31 +1,38 @@
 export default class TreebankDataItem {
   constructor (elem) {
     this.version = 0
+    this.app = null
     this.sourceUrl = null
     this.reference = null
 
     /*
       Treebank data on a page must have an element with the following obligatory data attributes:
-        data-alpheios_tb_version - a version of a data format (the latest version is 1);
-        data-alpheios_tb_src - a schema of a treebank template URL;
+        data-alpheios_tb_app - the only app currently supported is 'perseids-treebank-template'
+        data-alpheios_tb_app_version - a version of a data format (the latest version is 1);
+        data-alpheios_tb_app_url - a schema of a treebank template URL;
         data-alpheios_tb_ref - a reference that will be used to load data into the iframe initially
       Example:
-        data-alpheios_tb_version="3"
-        data-alpheios_tb_src="https://alpheios-project.github.io/treebank-template/embed/DOC/SENTENCE?w=WORD"
+        data-alpheios_tb_app="perseids-treebank-template"
+        data-alpheios_tb_app_version="1"
+        data-alpheios_tb_app_url="https://alpheios-project.github.io/treebank-template/embed/DOC/SENTENCE?w=WORD"
         data-alpheios_tb_ref="on-the-murder-of-eratosthenes-1-50#1-1"
 
       HTML elements that are surrounding words must have a `data-alpheios_tb_ref` ref attribute.
       It will tie a word to its position in a tree.
       */
-    const tbSrcElem = elem.ownerDocument.querySelector('[data-alpheios_tb_version]')
+    const tbSrcElem = elem.ownerDocument.querySelector('[data-alpheios_tb_app]')
     if (tbSrcElem) {
-      if (tbSrcElem.dataset.alpheios_tb_version) {
-        this.version = Number.parseInt(tbSrcElem.dataset.alpheios_tb_version, 10)
+      this.app = tbSrcElem.dataset.alpheios_tb_app
+      if (this.app !== 'perseids-treebank-template') {
+        throw new Error('Unsupported treebank application. This version of Alpheios only supports the perseids-treebank-template viewer app.')
+      }
+      if (tbSrcElem.dataset.alpheios_tb_app_version) {
+        this.version = Number.parseInt(tbSrcElem.dataset.alpheios_tb_app_version, 10)
         if (Number.isNaN(this.version)) { throw new Error(`Treebank version is incorrect in: ${tbSrcElem.outerHTML}`) }
       }
 
-      if (!tbSrcElem.dataset.alpheios_tb_src) { throw new Error(`Missing treebank source URL in: ${tbSrcElem.outerHTML}`) }
-      this.sourceUrl = tbSrcElem.dataset.alpheios_tb_src
+      if (!tbSrcElem.dataset.alpheios_tb_app_url) { throw new Error(`Missing treebank source URL in: ${tbSrcElem.outerHTML}`) }
+      this.sourceUrl = tbSrcElem.dataset.alpheios_tb_app_url
 
       const tbRefElem = elem.closest('[data-alpheios_tb_ref]')
       if (!tbRefElem || !tbRefElem.dataset.alpheios_tb_ref) {
@@ -52,6 +59,11 @@ export default class TreebankDataItem {
   }
 
   get hasTreebankData () {
-    return this.version > 0
+    if (this.app && this.version > 0 && this.sourceUrl) {
+      return true
+    } else  {
+      return false
+    }
+
   }
 }
--- packages/data-models/tests/treebank_data_item.test.js
+++ packages/data-models/tests/treebank_data_item.test.js
@@ -14,6 +14,7 @@ describe('treebank_data_item.test.js', () => {
     jest.spyOn(console, 'error')
     jest.spyOn(console, 'log')
     jest.spyOn(console, 'warn')
+    document.body.innerHTML = ''
   })
 
   afterEach(() => {
@@ -32,36 +33,67 @@ describe('treebank_data_item.test.js', () => {
     expect(item.hasTreebankData).toBeFalsy()
   })
 
-  it('2 TreebankDataItem - hasTreebankData is falsy if a reference is missing', () => {
+  it('2 TreebankDataItem - fail on reference missing', () => {
     const srcElem = document.createElement('div')
-    srcElem.setAttribute('data-alpheios_tb_src', 'http://example.org/DOC/SENTENCE/WORD')
+    srcElem.setAttribute('data-alpheios_tb_app','perseids-treebank-template')
+    srcElem.setAttribute('data-alpheios_tb_app_version', '1')
+    srcElem.setAttribute('data-alpheios_tb_app_url', 'http://example.org/DOC/SENTENCE/WORD')
     document.body.appendChild(srcElem)
     const wdElem = document.createElement('span')
     document.body.appendChild(wdElem)
-    const item = new TreebankDataItem(wdElem)
-    expect(item.hasTreebankData).toBeFalsy()
+    expect.assertions(1)
+    try {
+      const item = new TreebankDataItem(wdElem)
+    } catch(error) {
+      expect(error).toHaveProperty('message','Cannot find an element with the data-alpheios_tb_ref data attribute among the ancestors')
+    }
   })
 
-  it('3 TreebankDataItem - hasTreebankData is falsy on invalid reference', () => {
+  it('3 TreebankDataItem - fail on invalid reference', () => {
     const srcElem = document.createElement('div')
-    srcElem.setAttribute('data-alpheios_tb_src', 'http://example.org/DOC/SENTENCE/WORD')
+    srcElem.setAttribute('data-alpheios_tb_app','perseids-treebank-template')
+    srcElem.setAttribute('data-alpheios_tb_app_version', '1')
+    srcElem.setAttribute('data-alpheios_tb_app_url', 'http://example.org/DOC/SENTENCE/WORD')
     document.body.appendChild(srcElem)
     const wdElem = document.createElement('span')
     wdElem.setAttribute('data-alpheios_tb_ref', 'treebank')
     document.body.appendChild(wdElem)
-    const item = new TreebankDataItem(wdElem)
-    expect(item.hasTreebankData).toBeFalsy()
+    expect.assertions(1)
+    try {
+      const item = new TreebankDataItem(wdElem)
+    } catch(error) {
+      expect(error.message).toEqual(expect.stringContaining("Invalid treebank reference"))
+    }
+  })
+
+  it('4 TreebankDataItem - fail on invalid app', () => {
+    const srcElem = document.createElement('div')
+    srcElem.setAttribute('data-alpheios_tb_app','myapp')
+    srcElem.setAttribute('data-alpheios_tb_app_version', '1')
+    srcElem.setAttribute('data-alpheios_tb_app_url', 'http://example.org/DOC/SENTENCE/WORD')
+    document.body.appendChild(srcElem)
+    const wdElem = document.createElement('span')
+    wdElem.setAttribute('data-alpheios_tb_ref', 'treebank#1-1')
+    document.body.appendChild(wdElem)
+    expect.assertions(1)
+    try {
+      const item = new TreebankDataItem(wdElem)
+    } catch(error) {
+      expect(error.message).toEqual(expect.stringContaining("Unsupported treebank application."))
+    }
   })
 
   it('4 TreebankDataItem - constructs full item', () => {
     const srcElem = document.createElement('div')
-    srcElem.setAttribute('data-alpheios_tb_version', '1')
-    srcElem.setAttribute('data-alpheios_tb_src', 'http://example.org/DOC/SENTENCE/WORD')
+    srcElem.setAttribute('data-alpheios_tb_app','perseids-treebank-template')
+    srcElem.setAttribute('data-alpheios_tb_app_version', '1')
+    srcElem.setAttribute('data-alpheios_tb_app_url', 'http://example.org/DOC/SENTENCE/WORD')
     document.body.appendChild(srcElem)
     const wdElem = document.createElement('span')
     wdElem.setAttribute('data-alpheios_tb_ref', 'treebank#1-1')
     document.body.appendChild(wdElem)
     const item = new TreebankDataItem(wdElem)
+    console.info("TB ITEM",item)
     expect(item.provider).toEqual('http://example.org')
     expect(item.fullUrl).toEqual('http://example.org/treebank/1/1')
     expect(item.doc).toEqual('treebank')
