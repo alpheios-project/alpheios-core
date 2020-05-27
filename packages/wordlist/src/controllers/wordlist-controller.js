5c2539db67f7de6b386d8b855451395563f63ec4
Irina <irina060981@yandex.ru>
2020-05-27 22:00:59 +1000

    small fix

--- packages/wordlist/src/controllers/wordlist-controller.js
+++ packages/wordlist/src/controllers/wordlist-controller.js
@@ -253,9 +253,7 @@ export default class WordlistController {
   * Emits a WORDITEM_SELECTED event for the selected item
   */
   async selectWordItem (languageCode, targetWord) {
-    console.info('selectWordItem - started', languageCode, targetWord)
     let wordItem = this.getWordListItem(languageCode, targetWord, false)
-    console.info('selectWordItem - wordItem', wordItem)
     WordlistController.evt.WORDITEM_SELECTED.pub(wordItem)
   }
 
